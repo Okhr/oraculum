a721ca2364634bcff1d82bb152070c9b7c2db5ba
Florian Arnoud <florian.arnoud@gmail.com>
2024-02-28 17:11:35 +0000

    text extraction

---
+++ src/core/extraction.py
@@ -0,0 +1,73 @@
+import json
+import os
+import ebooklib
+from ebooklib import epub
+import bs4
+from bs4 import BeautifulSoup
+
+
+def extract_book_metadata(book: ebooklib.epub.EpubBook):
+    return {
+        'identifier': book.get_metadata('DC', 'identifier'),
+        'title': book.get_metadata('DC', 'title')[0][0],
+        'language': book.get_metadata('DC', 'language')[0][0],
+        'creator': book.get_metadata('DC', 'creator')[0][0]
+    }
+
+
+def extract_structured_content(book: ebooklib.epub.EpubBook):
+    def process_navpoint_recursive(navpoint):
+        result = {}
+        result['id'] = navpoint['id']
+        result['playorder'] = navpoint['playOrder']
+        result['label'] = navpoint.navLabel.text.strip()
+        result['content_path'] = navpoint.content['src']
+        result['content'] = parse_item(book, result['content_path'])
+
+        children_navpoints = navpoint.find_all('navPoint')
+        if len(children_navpoints) > 0:
+            result['children'] = [process_navpoint_recursive(
+                child) for child in children_navpoints]
+        else:
+            result['children'] = []
+
+        return result
+
+    navs = list(book.get_items_of_type(ebooklib.ITEM_NAVIGATION))
+    nav_html = navs[0].content.decode('utf-8')
+    nav_soup = BeautifulSoup(nav_html, features="xml")
+    navmap = nav_soup.navMap
+    print(navmap.prettify())
+
+    table_of_content = []
+
+    for child in navmap.children:
+        if child.name == 'navPoint':
+            table_of_content.append(process_navpoint_recursive(child))
+
+    return table_of_content
+
+
+def parse_item(book: ebooklib.epub.EpubBook, item_href: str):
+    print(item_href)
+    item = book.get_item_with_href(item_href)
+    item_body_content = item.get_body_content()
+    item_soup = BeautifulSoup(item_body_content, features='lxml')
+    item_text = item_soup.html.body.get_text().strip()
+    return item_text
+
+
+if __name__ == '__main__':
+    EBOOK_PATH = "data/epubs/Sorceleur - L'Integrale - Andrzej Sapkowski.epub"
+    book = epub.read_epub(EBOOK_PATH)
+
+    print(json.dumps(extract_structured_content(book), indent=4))
+
+    """
+    documents = list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
+    images = list(book.get_items_of_type(ebooklib.ITEM_IMAGE))
+    navigation = list(book.get_items_of_type(ebooklib.ITEM_NAVIGATION))
+    covers = list(book.get_items_of_type(ebooklib.ITEM_COVER))
+
+    parse_item("Sapkowski, Andrzej - Le Dernier Voeu_split_004.htm")
+    """
