64e057bb530403566115f268d1a8e20081735e27
Florian Arnoud <florian.arnoud@gmail.com>
2024-03-13 20:23:44 +0000

    tagging part 1 : NER

---
+++ src/core/tagging.py
@@ -0,0 +1,134 @@
+from collections import OrderedDict
+import json
+import os
+from pprint import pp
+import re
+import statistics
+from tqdm import tqdm
+from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
+
+
+class NamedEntityRecognition:
+    def __init__(self, model_name: str, entity_groups: list[str]) -> None:
+        self.entity_groups = entity_groups
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForTokenClassification.from_pretrained(
+            model_name)
+        self.nlp_pipeline = pipeline(
+            'ner',
+            model=self.model,
+            tokenizer=self.tokenizer,
+            aggregation_strategy='simple'
+        )
+
+    def _preprocess_text(self, text: str | list[str], splitting_regex: str, min_length: int = 0) -> list[str]:
+        text_parts = []
+
+        if isinstance(text, str):
+            text = [text]
+
+        print('Splitting content')
+
+        for t in text:
+            for t_part in re.split(splitting_regex, t):
+                t_part = t_part.strip() if t_part else ''
+                if len(t_part) > min_length:
+                    if len(t_part) > self.tokenizer.max_len_single_sentence:
+                        print(f'Warning : t_part length of {len(t_part)} is longer than max sentence length ({
+                              self.tokenizer.max_len_single_sentence}), although it is probably okey since the tokenizer will merge characters')
+                    text_parts.append(t_part)
+        return text_parts
+
+    def get_raw_tags(self, text: str | list[str]):
+        text_parts = self._preprocess_text(
+            text,
+            splitting_regex=r"(\\n)+|[.!?]",
+            min_length=10
+        )
+
+        tags = []
+
+        print('Extracting tags')
+        for t_part in tqdm(text_parts):
+            for tag in self.nlp_pipeline(t_part):
+                if tag['entity_group'] in self.entity_groups:
+                    tags.append({
+                        'entity_group': tag['entity_group'],
+                        'score': tag['score'],
+                        'word': tag['word']
+                    })
+        return tags
+
+    def get_grouped_tags(self, text: str | list[str]):
+        raw_tags = self.get_raw_tags(text)
+
+        print('Grouping tags')
+        grouped_tags = dict()
+        for tag in raw_tags:
+            entity_group = tag['entity_group']
+            word = tag['word']
+            if entity_group not in grouped_tags:
+                grouped_tags[entity_group] = {}
+            if word not in grouped_tags[entity_group]:
+                grouped_tags[entity_group][word] = {'scores': []}
+            grouped_tags[entity_group][word]['scores'].append(
+                float(tag['score'])
+            )
+            scores = grouped_tags[entity_group][word]['scores']
+            grouped_tags[entity_group][word]['count'] = len(scores)
+            grouped_tags[entity_group][word]['min'] = min(scores)
+            grouped_tags[entity_group][word]['max'] = max(scores)
+            grouped_tags[entity_group][word]['mean'] = statistics.mean(
+                scores) if len(scores) > 1 else scores[0]
+            grouped_tags[entity_group][word]['sdt'] = statistics.stdev(
+                scores) if len(scores) > 1 else 0
+            grouped_tags[entity_group][word]['median'] = statistics.median(
+                scores) if len(scores) > 1 else scores[0]
+
+        ordered_grouped_tags = OrderedDict()
+        for entity_group, name_dict in grouped_tags.items():
+            ordered_grouped_tags[entity_group] = OrderedDict(
+                sorted(
+                    name_dict.items(),
+                    key=lambda x: x[1]['count'],
+                    reverse=True
+                )
+            )
+        return OrderedDict(sorted(ordered_grouped_tags.items(), key=lambda x: x[0]))
+
+
+def load_book_raw_content(book_data: dict) -> list[str]:
+    content = []
+
+    def load_recursive(node: dict):
+        content.append(node['content'])
+        if node['children']:
+            for child in node['children']:
+                load_recursive(child)
+
+    for elem in book_data['data']:
+        load_recursive(elem)
+
+    return content
+
+
+if __name__ == '__main__':
+    ner = NamedEntityRecognition(
+        model_name='Jean-Baptiste/camembert-ner',
+        entity_groups=['PER', 'LOC', 'MISC', 'ORG']
+    )
+
+    for book_name in os.listdir('data/extracted_books'):
+        print(f'Loading book : {book_name.replace(".json", "")}')
+        with open(f'data/extracted_books/{book_name}') as f:
+            book_data = json.load(f)
+        raw_content = load_book_raw_content(book_data)
+        grouped_tags = ner.get_grouped_tags(raw_content)
+
+        if not os.path.exists('data/tags'):
+            os.makedirs('data/tags')
+
+        with open(f'data/tags/{book_name}', 'w') as f:
+            json.dump(grouped_tags, f, ensure_ascii=False, indent=4)
+
+        print()
